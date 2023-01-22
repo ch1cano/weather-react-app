05235a32719da3c00722b016a5424a6bcb630439
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-01-22 15:07:38 +0300

    work with component

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -5,31 +5,18 @@ import '../css/Search.css';
 const Search = () => {
   return (
     <div className="search">
-      <div className="search__body">
-        <div className="search__row">
-          <div className="search__header">
-            <div className="search__title title">
-              <p>Forecast</p>
-            </div>
-            <div className="current__location">
-              <img src="" alt="logo" />
-              <div className="current__location subtitle">Current Location</div>
-              <div className="current__location title">Krasnodar, Russia</div>
-            </div>
-          </div>
+      <div className="container">
+        <div className="logo">Forecast</div>
+        <div className="current-location">
+          <p className="current-location-text">Current Location</p>
+          <p className="current-location-city">Krasnodar, Russia</p>
         </div>
-        <div className="search__row">
-          <div className="search__title">
-            <h1>The Only Weather Forecast You Need</h1>
-          </div>
-          {/* Here Line */}
-          <div className="search__input">
-            <input type="text" />
-            <img src="" alt="search icon" />
-          </div>
-          <div className="search__pick-location">
-            <img src="" alt="dart icon" />
-          </div>
+      </div>
+      <div className="main">
+        <h1 className="main-title">The Only Weather Forecast You Need</h1>
+        <div className="container search-input">
+          <input type="text" className="input-search" placeholder="Enter location" />
+          <button className="search-button">search</button>
         </div>
       </div>
     </div>
