59e4a2ab7063d4b4c64fa92de3d5a491edbfb66f
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-01-22 12:06:41 +0300

    add search component

---
+++ src/Components/Search.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div className="search">
+      <div className="search__body">
+        <div className="search__header">
+          <div className="search__title title">
+            <p>Forecast</p>
+          </div>
+          <div className="current__location">
+            <img src="" alt="logo" />
+            <div className="current__location subtitle">Current Location</div>
+            <div className="current__location title">Krasnodar, Russia</div>
+          </div>
+        </div>
+        <div className="search__row">
+          <div className="search__title">
+            <p>The Only Weather Forecast You Need</p>
+          </div>
+          {/* Here Line */}
+          <div className="search__input">
+            <input type="text" />
+            <img src="" alt="search icon" />
+          </div>
+          <div className="search__pick-location">
+            <img src="" alt="dart icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
