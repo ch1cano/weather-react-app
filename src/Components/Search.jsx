85e35809e86d53663843e3826b3d426e500c4810
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-01-22 12:51:26 +0300

    add assets and some styles

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
+import '../css/Search.css';
+
 const Search = () => {
   return (
     <div className="search">
       <div className="search__body">
-        <div className="search__header">
-          <div className="search__title title">
-            <p>Forecast</p>
-          </div>
-          <div className="current__location">
-            <img src="" alt="logo" />
-            <div className="current__location subtitle">Current Location</div>
-            <div className="current__location title">Krasnodar, Russia</div>
+        <div className="search__row">
+          <div className="search__header">
+            <div className="search__title title">
+              <p>Forecast</p>
+            </div>
+            <div className="current__location">
+              <img src="" alt="logo" />
+              <div className="current__location subtitle">Current Location</div>
+              <div className="current__location title">Krasnodar, Russia</div>
+            </div>
           </div>
         </div>
         <div className="search__row">
           <div className="search__title">
-            <p>The Only Weather Forecast You Need</p>
+            <h1>The Only Weather Forecast You Need</h1>
           </div>
           {/* Here Line */}
           <div className="search__input">
